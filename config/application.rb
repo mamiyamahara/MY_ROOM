f9aa25454558591e2b27ff6283286253c90ae4ca
vagrant <vagrant@localhost.localdomain>
2020-10-11 11:30:17 +0000

    [Add] Gemfile

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,10 @@ module MYRoom
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    #エラーメッセージをja.ymlファイルを用いて日本語化
+    config.i18n.default_locale = :ja
+    # config/locales以下のディレクトリ内にある全てのymlファイルを読み込むように指示する一文
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.yml').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
