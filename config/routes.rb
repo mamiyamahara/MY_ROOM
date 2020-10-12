4eef81583729bd8f6f2b33201f2c7dd13676ebb1
vagrant <vagrant@localhost.localdomain>
2020-10-12 08:16:23 +0000

    [Add] controller/routes/model/view/ template

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+
+  root 'homes#top'
+  get 'homes/about'
   devise_for :users
+
+  resources :users, only: [:show, :edit, :update, :destroy] do
+    resource :relationships, only: [:create, :destroy]
+    get 'follows' => 'relationships#follower', as: 'follows'
+    get 'followers' => 'relationships#followed', as: 'followers'
+  end
+
+  resources :posts do
+    resource :favorites, only: [:create, :destroy]
+    get 'likes' => 'favorites#likes', as: 'likes'
+    get 'liked' => 'favorites#liked', as: 'liked'
+    resource :post_comments, only: [:create]
+    resources :post_comments, only: [:destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,70 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+	[
+		{
+	        id: "2",
+	        email: "test1@test.com",
+	        name: "テスト",
+	        password: "111111",
+	        password_confirmation: "111111",
+	        profile_image: open("./app/assets/images/profile_image.jpeg"),
+	        introduction: "一人暮らし歴1年目です。よろしくお願いします。",
+	        layout: "1DK",
+	        size: "18㎡"
+        },
+
+        {
+	        id: "3",
+	        email: "test2@test.com",
+	        name: "テスト2",
+	        password: "222222",
+	        password_confirmation: "222222",
+	        profile_image: open("./app/assets/images/profile_image2.jpeg"),
+	        introduction: "はじめまして！来年から一人暮らし予定で、参考になるインテリアを探しています。よろしくお願いします。",
+	        layout: "1DK(予定)",
+	        size: "18㎡(予定)"
+        },
+
+        {
+	        id: "4",
+	        email: "test3@test.com",
+	        name: "テスト3",
+	        password: "333333",
+	        password_confirmation: "333333",
+	        profile_image: open("./app/assets/images/profile_image3.jpeg"),
+	        introduction: "はじめまして。大学入学をきっかけに、去年から一人暮らしをはじめました。最近は植物大好きで色々育てています。一人暮らしする前はまだイメージがついていなかったので、ベランダのない部屋を選んでしまい、少し後悔しています(笑)",
+	        layout: "1DK",
+	        size: "16㎡"
+        },
+    ]
+    )
+
+Post.create!(
+	[
+		{
+        	user_id: "2",
+	        id: "1",
+	        title: "モノトーンインテリア",
+	        body: "こだわりポイントはテレビを見ながら寝られるソファの配置です。映画が好きなので、気づいたら観ながらソファで寝てしまっていることもしばしば。見えませんが、一番手前にベッドもあります。テレビの配線をもう少しきれいにしたいので、いい案があればコメントください。",
+	        post_image: open("./app/assets/images/interior_photo1.jpg")
+        },
+
+        {
+        	user_id: "2",
+	        id: "2",
+	        title: "キッチンの収納",
+	        body: "あまり物を置きたくないのですが、すぐ使えるようにグラスと調味料だけは置いています。調理器具は全てステンレスにして統一感を出しています。",
+	        post_image: open("./app/assets/images/kitchen_interior.jpg")
+        },
+
+		{
+        	user_id: "4",
+	        id: "1",
+	        title: "ウッディ調インテリア？",
+	        body: "暖かい感じのインテリアが好きなので、木×緑なインテリアになるように意識しています。予算的にあまり新しい家具を買ったりする余裕がなかったので、ドレッサーと椅子は実家にあった物を持ってきてそのまま使っています(笑)",
+	        post_image: open("./app/assets/images/interior_photo2.jpg")
+        },
+    ]
+    )
