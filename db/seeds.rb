972ffeadfa2a777780adc9da84bbddcf44061e71
vagrant <vagrant@localhost.localdomain>
2020-10-20 11:37:46 +0000

    [Fix] production.rb2

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ User.create!(
 	        name: "テスト",
 	        password: "111111",
 	        password_confirmation: "111111",
-	        profile_image: File.open("./app/assets/images/profile_image.jpeg"),
+	        profile_image: File.open("./app/assets/images/profile_image.jpg"),
 	        introduction: "一人暮らし歴1年目です。よろしくお願いします。",
 	        layout: "1R",
 	        size: "18㎡"
@@ -25,7 +25,7 @@ User.create!(
 	        name: "テスト2",
 	        password: "222222",
 	        password_confirmation: "222222",
-	        profile_image: File.open("./app/assets/images/profile_image2.jpeg"),
+	        profile_image: File.open("./app/assets/images/profile_image2.jpg"),
 	        introduction: "はじめまして！来年から一人暮らし予定で、参考になるインテリアを探しています。よろしくお願いします。",
 	        layout: "1DK(予定)",
 	        size: "18㎡(予定)"
@@ -37,7 +37,7 @@ User.create!(
 	        name: "テスト3",
 	        password: "333333",
 	        password_confirmation: "333333",
-	        profile_image: File.open("./app/assets/images/profile_image3.jpeg"),
+	        profile_image: File.open("./app/assets/images/profile_image3.jpg"),
 	        introduction: "はじめまして。大学入学をきっかけに、去年から一人暮らしをはじめました。最近は植物大好きで色々育てています。一人暮らしする前はまだイメージがついていなかったので、ベランダのない部屋を選んでしまい、少し後悔しています(笑)",
 	        layout: "1K",
 	        size: "16㎡"
@@ -52,7 +52,9 @@ User.create!(
 # 	        id: "1",
 # 	        title: "モノトーンインテリア",
 # 	        body: "こだわりポイントはテレビを見ながら寝られるソファの配置です。映画が好きなので、気づいたら観ながらソファで寝てしまっていることもしばしば。見えませんが、一番手前にベッドもあります。テレビの配線をもう少しきれいにしたいので、いい案があればコメントください。",
-# 	        post_image: File.open("./app/assets/images/interior_photo1.jpg")
+# 	        post_image: File.open("./app/assets/images/interior_photo1.jpg"),
+# 	        created_at: "2020-10-20",
+# 	        updated_at: "2020-10-20",
 #         },
 
 #         {
@@ -60,7 +62,9 @@ User.create!(
 # 	        id: "2",
 # 	        title: "キッチンの収納",
 # 	        body: "あまり物を置きたくないのですが、すぐ使えるようにグラスと調味料だけは置いています。調理器具は全てステンレスにして統一感を出しています。",
-# 	        post_image: File.open("./app/assets/images/kitchen_interior.jpg")
+# 	        post_image: File.open("./app/assets/images/kitchen_interior.jpg"),
+# 	        created_at: "2020-10-21",
+# 	        updated_at: "2020-10-21",
 #         },
 
 # 		{
@@ -68,7 +72,9 @@ User.create!(
 # 	        id: "1",
 # 	        title: "ウッディ調インテリア？",
 # 	        body: "暖かい感じのインテリアが好きなので、木×緑なインテリアになるように意識しています。予算的にあまり新しい家具を買ったりする余裕がなかったので、ドレッサーと椅子は実家にあった物を持ってきてそのまま使っています(笑)",
-# 	        post_image: File.open("./app/assets/images/interior_photo2.jpg")
+# 	        post_image: File.open("./app/assets/images/interior_photo2.jpg"),
+# 	        created_at: "2020-10-21",
+# 	        updated_at: "2020-10-21",
 #         },
 #     ]
 #     )
