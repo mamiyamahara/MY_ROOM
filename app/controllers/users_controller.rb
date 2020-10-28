2203ac4bd20bc7ff02fcc4a98e96e8ab32c9cf34
vagrant <vagrant@localhost.localdomain>
2020-10-28 07:23:53 +0000

    [Add] Follow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.page(params[:page]).per(10).order("updated_at DESC")
